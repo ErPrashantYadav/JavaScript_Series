9fd04504c257c991a438f52c03a8635b7df0416a
PRASHANT YADAV <143265709+ErPrashantYadav@users.noreply.github.com>
2024-10-01 14:38:56 +0000

    multiple map and filter uses

---
+++ 05_iterations/map_filter.js
@@ -0,0 +1,16 @@
+//we can use multiple map and filter
+
+// note: in map function 
+// () => ()  if curly bracket is not used then no return is used
+// () => ({})  if curly bracket is used then return is used
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumers.map( (num) => { return num + 10})
+
+const newNums = myNumers
+                .map((num) => num * 10 )
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+
+console.log(newNums);
